8dacf0b3f8324249e0fd3aece8252b8ef2e4f6bd
Anton <antonberglund_92@hotmail.com>
2022-01-19 23:17:14 +0100

    get data from local backend

--- src/view/CommunityView.js
+++ src/view/CommunityView.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import Axios from 'axios'
 
 export const CommunityView = () => {
+
+    const fetchDataFromBackend = () => {
+        Axios.get('http://localhost:3001/user')
+        .then((response) => {
+            console.log(response)
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+    }
+
     return (
         <div>
+            <button onClick={() => fetchDataFromBackend()}>Testar backend</button>
             <h1>Forum</h1>
         </div>
     )
