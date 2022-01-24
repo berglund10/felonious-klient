2e5b27ed4a45422db9346d58d618ccee8728f7cb
Anton <antonberglund_92@hotmail.com>
2022-01-24 14:19:48 +0100

    rundor uppdateras efter 10 sek

--- src/components/profile/ProfileEgenskaper.js
+++ src/components/profile/ProfileEgenskaper.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Axios from 'axios'
 
 export const ProfileEgenskaper = () => {
@@ -6,43 +6,63 @@ export const ProfileEgenskaper = () => {
 
     const [health, setHealth] = useState()
     const [strength, setStrength] = useState()
+    const [rundor, setRundor] = useState()
+    const [character_id, setCharacter_id] = useState()
     const username = localStorage.getItem('username')
-    
+
+    const MINUTE_MS = 10000;
+
+    useEffect(() => {
+        getUserInfo()
+        const interval = setInterval(() => {
+            updateRundor()
+        }, MINUTE_MS);
+
+        return () => clearInterval(interval);
+    }, [rundor])
+
+    const updateRundor = () => {
+        getUserInfo()
+        Axios.put(`http://localhost:3001/char/${character_id}`).then(() => {
+            console.log("Rundor uppdaterade")
+        })
+    }
+
     const getUserInfo = () => {
         Axios.get(`http://localhost:3001/searchuser?username=${username}`)
-        .then((response) => {
-            getCharInfo(response.data[0].character)
-        })
-        .catch((error) => {
-            console.log(error)
-        })
+            .then((response) => {
+                setCharacter_id(response.data[0].character)
+                getCharInfo(response.data[0].character)
+            })
+            .catch((error) => {
+                console.log(error)
+            })
     }
 
     const getCharInfo = (character_id) => {
         Axios.get(`http://localhost:3001/char/${character_id}`).then((res) => {
             setHealth(res.data.health)
             setStrength(res.data.strength)
+            setRundor(res.data.rundor)
         })
     }
 
 
 
     // hämta charID sen hämta information om den och visa här
-    return(
+    return (
         <div>
             Detta är dina egenskaper:
             <button onClick={() => getUserInfo()}>Hämta info</button>
-            <br/>
-            <br/>
+            <br />
+            <button onClick={() => updateRundor()}>Update rundor</button>
+            <br />
             Hälsa: {health}
-            <br/>
-            Uttålighet:
-            <br/>
+            <br />
+            Rundor: {rundor}
+            <br />
             Stryka: {strength}
-            <br/>
-            Karisma:
-            <br/>
-            Undvika anfall:
+            <br />
 
         </div>
     )
