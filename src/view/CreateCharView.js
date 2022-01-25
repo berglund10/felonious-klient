04f04f8127b581f54df87117b377c347aa509fcc
Anton <antonberglund_92@hotmail.com>
2022-01-25 13:55:02 +0100

    kan bara skapa char om du inte redan har en

--- src/view/CreateCharView.js
+++ src/view/CreateCharView.js
@@ -1,6 +1,7 @@
-import React , {useState} from 'react'
+import React, { useState, useContext } from 'react'
 import Axios from 'axios'
 import { useNavigate } from 'react-router-dom'
+import { UserContext } from '../shared/global/UserProvider'
 
 export const CreateCharView = () => {
 
@@ -8,27 +9,38 @@ export const CreateCharView = () => {
     const [strength, setStrength] = useState()
     const history = useNavigate()
 
+    const [authUser, setAuthUser] = useContext(UserContext);
+
+
+    const displayUserIfAuth = () => {
+        return (authUser)
+            ? <div><span>Health:  </span><input onChange={(event => setHealth(event.target.value))}></input> <br />
+                <span>Strength:  </span><input onChange={(event => setStrength(event.target.value))}></input> <br />
+                <button onClick={() => createCharacter()}>Skapa char</button></div>
+            : <h3>Du måste vara inloggad för att få mer information</h3>
+    }
+
     const username = localStorage.getItem('username')
 
     const createCharacter = () => {
         Axios.get(`http://localhost:3001/searchuser?username=${username}`, {
         })
-        .then((response) => {
-            Axios.post(`http://localhost:3001/char/${response.data[0]._id}`, {
-                health: health,
-                strength: strength,
+            .then((response) => {
+                if(response.data[0].character === undefined) {
+                    Axios.post(`http://localhost:3001/char/${response.data[0]._id}`, {
+                        health: health,
+                        strength: strength,
+                    })
+                }
             })
-        })
-        .then(() => {
-            history('/game')
-        })
+            .then(() => {
+                history('/game')
+            })
     }
 
     return (
         <div>
-                <span>Health:  </span><input onChange={(event => setHealth(event.target.value))}></input> <br/>
-                <span>Strength:  </span><input onChange={(event => setStrength(event.target.value))}></input> <br/>
-                <button onClick={() => createCharacter()}>Skapa char</button>
+            {displayUserIfAuth()}
         </div>
     )
 }
