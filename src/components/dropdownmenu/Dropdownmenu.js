24d53240be7d191b9865ee767583b60055dca6a4
Anton <antonberglund_92@hotmail.com>
2022-02-18 18:01:24 +0100

    metod för kneg

--- src/components/dropdownmenu/Dropdownmenu.js
+++ src/components/dropdownmenu/Dropdownmenu.js
@@ -1,17 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Axios from 'axios';
 
 export const Dropdownmenu = () => {
+    const [value, setValue] = useState(0);
+    const username = localStorage.getItem('username')
+
+    const knega = (rundor) => {
+        Axios.put('http://localhost:3001/kneg', {
+            username: username,
+            rundor: rundor
+        }).then((res) => {
+            console.log(res)
+        }).catch((error) => {
+            console.log(error)
+        })
+    }
+
     return (
         <div>
-            <select>
+            <select onChange={(e) => setValue(e.target.value)}>
+                <option value="0">0 rundor</option>
                 <option value="10">10 rundor</option>
                 <option value="25">25 rundor</option>
-                <option selected value="50">50 rundor</option>
+                <option value="50">50 rundor</option>
                 <option value="100">100 rundor</option>
             </select>
-            <br/>
-            <br/>
-            <button>Jobba</button>
+            <br />
+            <br />
+            <button onClick={() => knega(value)}>Jobba</button>
         </div>
     )
 }
